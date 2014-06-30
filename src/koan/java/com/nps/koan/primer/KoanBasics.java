20c33c697a64de9119076c061d700a756773e57b
unknown <nicholas_smith@UK017ZFNX4J.mhf2.mhf.mhc>
2014-06-30 11:32:23 +0100

    Tidy up of Koan Primer

--- src/koan/java/com/nps/koan/primer/KoanBasics.java
+++ src/koan/java/com/nps/koan/primer/KoanBasics.java
@@ -22,14 +22,13 @@ public class KoanBasics {
     /**
      * Grasshoppers should meditate on how to make the product int equal to 50.
      */
-    @Koan  @Enlighten
+    @Koan
     public void reflectOnTheProductOfIAndJ(){
         int i = 10;
         int j = 5;
         int product = 0;
 
         /* (@_@) Your code starts here */
-        product = i * j;
         /* (^_^) Your code ends here */
 
         assertThat(product, is(50));
@@ -38,14 +37,14 @@ public class KoanBasics {
     /**
      * Grasshoppers should use this koan to understand how to reach enlightenment
      */
-    @Koan
+    @Koan @Enlighten
     public void reflectOnProvidingTheSolutionToTheProductOfIAndJ(){
         int i = 10;
         int j = 5;
         int product = 0;
 
         /* (@_@) */
-        product = i * j;
+
         /* (^_^) */
 
         assertThat(product, is(50));
